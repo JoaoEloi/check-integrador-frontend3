62a533daa4ea940b15df06848d8b484aa48efb60
JoaoEloi <ejoao605@gmail.com>
2022-04-05 16:54:37 -0300

    corrigindo o SASS

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,14 @@
+import './header.scss'
+import Button from 'react-bootstrap/Button'
+
+
+export default function Header() {
+    return(
+
+        <header className="header_container">
+            <Button variant="dark">Dark</Button>{' '}
+        </header>
+
+    )
+
+} 

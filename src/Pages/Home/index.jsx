e3f4e04b1d1f46198a6e857a6efd4e8d9f7b201a
Doug <86627627+Dougsss@users.noreply.github.com>
2022-04-10 15:44:52 -0300

    Ajustes Carrousel

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -13,51 +13,55 @@ export default function Home() {
             <div className="title">
                 <h1>Home</h1>
             </div>
-        <section className="carousel_container"> 
-            <Carousel >
-                
-                    <Carousel.Item interval={1000} >
-                    <div className="carousel_item">
-                        <img
-                            className="carousel_img"
-                            src={card_image}
-                            alt="First slide"
-                        />
-                        <Carousel.Caption>
-                            <h3 className="carousel_text" >First slide label</h3>
-                            <p className="carousel_text" >Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
-                    </div>
-                    </Carousel.Item>
+            <section className="carousel_container">
+                <Carousel >
 
                     <Carousel.Item interval={1000} >
-                        <img
-                            className="carousel_img"
-                            src={card_image}
-                            alt="Second slide"
-                        />
-
-                        <Carousel.Caption>
-                            <h3 className="carousel_text" >Second slide label</h3>
-                            <p className="carousel_text" >Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </Carousel.Caption>
+                        <div className="carousel_item">
+                            <img
+                                className="carousel_img"
+                                src={card_image}
+                                alt="First slide"
+                            />
+                            <Carousel.Caption>
+                                <h3 className="carousel_text" >First slide label</h3>
+                                <p className="carousel_text" >Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption>
+                        </div>
                     </Carousel.Item>
 
-                    <Carousel.Item interval={1000} > 
-                        <img
-                            className="carousel_img"
-                            src={card_image}
-                            alt="Third slide"
-                        />
+                    <Carousel.Item interval={1000} >
+                        <div className="carousel_item">
+                            <img
+                                className="carousel_img"
+                                src={card_image}
+                                alt="Second slide"
+                            />
 
-                        <Carousel.Caption>
-                            <h3 className="carousel_text" >Third slide label</h3>
-                            <p className="carousel_text" >Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                        </Carousel.Caption>
+                            <Carousel.Caption>
+                                <h3 className="carousel_text" >Second slide label</h3>
+                                <p className="carousel_text" >Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </Carousel.Caption>
+                        </div>
                     </Carousel.Item>
-                
-            </Carousel>
-        </section>
+
+                    <Carousel.Item interval={1000} >
+                        <div className="carousel_item">
+                            <img
+                                className="carousel_img"
+                                src={card_image}
+                                alt="Third slide"
+                            />
+
+                            <Carousel.Caption>
+                                <h3 className="carousel_text" >Third slide label</h3>
+                                <p className="carousel_text" >Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            </Carousel.Caption>
+                        </div>
+                    </Carousel.Item>
+
+                </Carousel>
+            </section>
 
             <div id="body" className="container_card_home">
                 {

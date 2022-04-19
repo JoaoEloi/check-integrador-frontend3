fd7ba6905b91c7b85abbb0c336f1c978b631b4c2
Marlusi <maimarbrito@gmail.com>
2022-04-19 12:28:30 -0300

    Subindo branch, modificação do aboutUs

--- src/Pages/AboutUs/index.jsx
+++ src/Pages/AboutUs/index.jsx
@@ -1,11 +1,24 @@
 import './aboutus.scss'
+import Button from 'react-bootstrap/Button'
+import { Card } from 'react-bootstrap';
+
 
 export default function AboutUs() {
     return (
-
+        
+        <div className="conteiner_aboutUs">
         <div className="title">
             <h1>Sobre nós</h1>
         </div>
-
+        <Card className="card_home_item">
+        <Card.Img src={item.image} className="img_card" alt="Foto do Card" />
+        <Card.Body className="card_body">
+            <Card.Title className='card_title'>{item.title}</Card.Title>
+            <Card.Text>{item.description}</Card.Text>
+            <Button onClick={addToCart} className="buton_item">{item.button}</Button>
+        </Card.Body>
+        </Card>
+        </div>
+        
     )
 }

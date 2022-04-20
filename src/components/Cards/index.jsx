c93153f2165f07eb2cd3d2c6e91cb2c1124fbcdc
Anderson Ribeiro <andersonsiilvar@gmail.com>
2022-04-20 11:15:38 -0300

    Pequena mudança

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -12,15 +12,9 @@ export default function Cards({ item }) {
         <Card className="card_home_item">
             <Card.Img src={item.imagem} className="img_card" alt="Foto do Card" />
             <Card.Body className="card_body">
-<<<<<<< HEAD
                 <Card.Title className='card_title'>{item.nome}</Card.Title>
                 <Card.Text>{item.descricao}</Card.Text>
-                <Button onClick={addToCart} className="buton_item">Adicionar</Button>
-=======
-                <Card.Title className='card_title'>{item.title}</Card.Title>
-                <Card.Text>{item.description}</Card.Text>
-                <Button onClick={() => addProducToCart(item.id)} className="buton_item">{item.button}</Button>
->>>>>>> befb36286b42e18ae78ea4a2e7e0d79b99e52069
+                <Button /*onClick={}*/ className="buton_item">Adicionar</Button>
             </Card.Body>
         </Card>
     )

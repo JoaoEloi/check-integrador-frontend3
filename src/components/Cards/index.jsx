befb36286b42e18ae78ea4a2e7e0d79b99e52069
Doug <86627627+Dougsss@users.noreply.github.com>
2022-04-20 10:20:52 -0300

    adicionando itens carrinho

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,20 +1,20 @@
-import './cards.scss'
-import Button from 'react-bootstrap/Button'
+import './cards.scss';
+import Button from 'react-bootstrap/Button';
 import { Card } from 'react-bootstrap';
-
+import React, { useContext } from 'react';
+import { CartContext } from '../../context/cart';
 
 export default function Cards({ item }) {
-    const addToCart = () => {
-        console.log("Adicionado!")
-    }
-    
+    const { addProducToCart } = useContext(CartContext);
+
+
     return (
         <Card className="card_home_item">
             <Card.Img src={item.image} className="img_card" alt="Foto do Card" />
             <Card.Body className="card_body">
                 <Card.Title className='card_title'>{item.title}</Card.Title>
                 <Card.Text>{item.description}</Card.Text>
-                <Button onClick={addToCart} className="buton_item">{item.button}</Button>
+                <Button onClick={() => addProducToCart(item.id)} className="buton_item">{item.button}</Button>
             </Card.Body>
         </Card>
 

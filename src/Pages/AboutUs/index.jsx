d76c599d877e7fcf9627aa4ea4df2141635e26a9
Mar Brito <maimarbrito@gmail.com>
2022-04-19 22:13:31 -0300

    AboutUs quebrado

--- src/Pages/AboutUs/index.jsx
+++ src/Pages/AboutUs/index.jsx
@@ -1,25 +1,31 @@
-import './aboutus.scss'
-import Button from 'react-bootstrap/Button'
-import { Card } from 'react-bootstrap';
+// import './aboutus.scss'
+// import Button from 'react-bootstrap/Button'
+// import { Card } from 'react-bootstrap';
+// import { Link } from '@mui/material';
 
 
-export default function AboutUs() {
-    return (
+// export default function AboutUs({item}) {
+//     return (
         
-        <div className="conteiner_aboutUs">
-        <div className="title">
-            <h1>Sobre nós</h1>
-            <h2>About Us teste da mar</h2>
-        </div>
-        <Card className="card_home_item">
-        <Card.Img className="img_card" alt="Foto do Card" />
-        <Card.Body className="card_body">
-            <Card.Title className='card_title'></Card.Title>
-            <Card.Text></Card.Text>
-            <Button className="buton_item"></Button>
-        </Card.Body>
-        </Card>
-        </div>
+//         <div className="conteiner_aboutUs">
+//         <div className="title">
+//             <h1>Sobre nós</h1>
+//             <h2>About Us teste da mar</h2>
+//         {/* </div>
+//         <Card className="card_home_item">
+//         <Card.Img className="img_card" alt="Foto do Card" />
+//         <Card.Body className="card_body">
+//             <Card.Title className='card_title'>{item.title}</Card.Title>
+//             <Card.Text>{item.description}</Card.Text>
+//             <Link href={item.link1}>
+//                 <Button className="button_item">{item.button1}</Button>
+//             </Link>
+//             <Link href={item.link2}>
+//                 <Button className="button_item">{item.button2}</Button>
+//             </Link>
+//         </Card.Body>
+//         </Card> */}
+//         </div>
         
-    )
-}
+//     )
+// }
---
+++ src/data/dataPerfil.jsx
@@ -0,0 +1,79 @@
+export const DataPerfil = [
+    {
+        id: 1,
+        title: 'Anderson Bueno',
+        description: 'Pão Especial, Maionese da Casa, Hambúrguer prensado, Queijo Cheddar, Alface e Tomate.',
+        button1: 'GitHub',
+        link1:'https://github.com/MarBrito',
+        button2: 'Likedin',
+        link2:'https://www.linkedin.com/in/marlusibrito/',
+        image: 'http://www.lepingue.com.br/admin/uploads/images/galeria/smash-salada_6afef15b97b53b06a934b4f5fdd0bfc0__800x600__0x0.jpg',
+
+    },
+    {
+        id: 2,
+        title: 'Anderson Ribeiro',
+        description: 'Pão e Maionese da Casa, Hambúrguer de Fraldinha, Queijo Muçarela, Molho de Mostarda e Rúcula.',
+        button1: 'GitHub',
+        link1:'https://github.com/MarBrito',
+        button2: 'Likedin',
+        link2:'https://www.linkedin.com/in/marlusibrito/',
+        image: 'http://www.lepingue.com.br/admin/uploads/images/galeria/fraldinha-na-mostarda_1a03f2943ef896758c14801580a5943d__800x600__0x0.jpg',
+
+    },
+    {
+        id: 3,
+        title: 'Douglas Araújo',
+        description: 'Com gás ou sem gás? Refresque-se com sua água favorita.',
+        button1: 'GitHub',
+        link1:'https://www.github.com/Dougsss',
+        button2: 'Likedin',
+        link2:'https://linkedin.com/in/doug-araujo',
+        image: 'http://www.lepingue.com.br/admin/uploads/images/galeria/agua-com-e-sem-gas_19b3d1073e4ebc83e6392331f4d11565__800x600__0x0.jpg',
+    },
+    {
+        id: 4,
+        title: 'João Eduardo Eloi',
+        description: 'Cortadas em forma de anéis, empanadas com uma leve camada de farinha de rosca já temperada. Acompanha Maionese de Pimenta.',
+        button1: 'GitHub',
+        link1:'https://github.com/JoaoEloi',
+        button2: 'Likedin',
+        link2:'https://www.linkedin.com/in/jo%C3%A3o-eduardo-da-concei%C3%A7%C3%A3o-eloi-533a19187/',
+        image: 'http://www.lepingue.com.br/admin/uploads/images/galeria/aneis-de-cebola_b0f12dba730bce1241701293db481579__800x600__0x0.jpg',
+
+    },
+    {
+        id: 5,
+        title: 'Lelia Salles',
+        description: 'teste teste teste teste',
+        button1: 'GitHub',
+        link1:'https://www.github.com/leliasalles',
+        button2: 'Likedin',
+        link2:'https://www.linkedin.com/in/leliasalles',
+        image: 'http://saboritaliana.com.br/imgs/manutencao.jpg',
+
+    },
+    {
+        id: 6,
+        title: 'Mar Brito',
+        description: 'teste teste teste teste',
+        button1: 'GitHub',
+        link1:'https://github.com/MarBrito',
+        button2: 'Likedin',
+        link2:'https://www.linkedin.com/in/marlusibrito/',
+        image: 'http://saboritaliana.com.br/imgs/manutencao.jpg',
+
+    },
+    {
+        id: 7,
+        title: 'Octavio',
+        description: 'teste teste teste teste',
+        button1: 'GitHub',
+        link1:'https://github.com/MarBrito',
+        button2: 'Likedin',
+        link2:'https://www.linkedin.com/in/marlusibrito/',
+        image: 'http://saboritaliana.com.br/imgs/manutencao.jpg',
+
+    },
+    
+]
